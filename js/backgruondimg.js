69eaa8f13bb70f2652849b6c5cba6a516d397d82
jiny <jinheechoi93@gmail.com>
2023-09-18 01:54:31 +0900

    img setting

--- js/backgruondimg.js
+++ js/backgruondimg.js
@@ -1,8 +1,8 @@
 const images = [
-    // "0.jpg", "1.jpg","2.jpg", //풍경
-    "3.jpg", "4.jpg", "19.jpg", "20.jpg",//지니
+    "0.jpg", "1.jpg","2.jpg", //풍경
+    //"3.jpg", "4.jpg", "19.jpg", "20.jpg",//지니
     //"21.jpg", "22.jpg","24.jpg",//엄마
-    "5.jpg","6.jpg","7.jpg","8.jpg","11.jpg","15.jpg","17.jpg","19.jpg", //세령 
+    //"5.jpg","6.jpg","7.jpg","8.jpg","11.jpg","15.jpg","17.jpg","19.jpg", //세령 
     //"9.jpg", "12.jpg","13.jpg","18.jpg", "24.jpg","25.jpg","26.jpg","25.jpg", //아인
     //"28.jpg","16.jpg","14.jpg","10.jpg", //친구들
 
